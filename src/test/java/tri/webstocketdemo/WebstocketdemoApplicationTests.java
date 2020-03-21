26b303cc48cf0e87401dbfba8879489c027e738e
Tri Vo <tri.minh.vo@mgm-tp.com>
2020-03-21 14:21:42 +0700

    Init app

---
+++ src/test/java/tri/webstocketdemo/WebstocketdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package tri.webstocketdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebstocketdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
